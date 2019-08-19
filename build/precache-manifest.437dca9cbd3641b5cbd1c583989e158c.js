c8512b1cc563a1982075850397f54be14136f957
Zach Hancock <zhancock@edx.org>
2019-08-19 09:52:44 -0400

    add ui build for now

---
+++ build/precache-manifest.437dca9cbd3641b5cbd1c583989e158c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04f5eb088e49a01a94840dafbe3b54cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55db7c02f4e4bea0436c",
+    "url": "/static/css/main.36a98a9f.chunk.css"
+  },
+  {
+    "revision": "4330f1aacba8683f5679",
+    "url": "/static/js/2.209ea4d1.chunk.js"
+  },
+  {
+    "revision": "55db7c02f4e4bea0436c",
+    "url": "/static/js/main.becfab0c.chunk.js"
+  },
+  {
+    "revision": "8d42aa4ce72933fbab0d",
+    "url": "/static/js/runtime~main.ec87e212.js"
+  }
+]);
